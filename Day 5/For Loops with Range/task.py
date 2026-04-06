b43cd4da671742343fc2f77c44fa03059bca8b53
ummam Tahreem <ummamtahreem@gmail.com>
2026-04-06 13:39:53 +0530

    adding all files

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,12 @@
+print(range(1,10))
+
+for number in range(1,10):
+    print(number)
+
+for number in range(1,11):
+    print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
---
+++ Day 7/task/main.py
@@ -0,0 +1,6 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
